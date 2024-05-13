43642b906ecd587cfd4a0afe357400fb491e68fd
Junrybuenavista <42229618+Junrybuenavista@users.noreply.github.com>
2024-05-13 19:08:42 +0800

    aa

---
+++ src/router/index.js
@@ -0,0 +1,30 @@
+import { createRouter,  createWebHistory } from "vue-router";
+import dashboard from '../pages/master/Master_Dashboard'
+import HomePage from "@/pages/HomePage.vue";
+import ProfilePage from "@/pages/ProfilePage.vue";
+
+const routes = [
+    {
+        name: 'Master_Dashboard',
+        path: '/',
+        component: dashboard
+    },
+    {
+        name: 'Home',
+        path: '/home',
+        component: HomePage
+    },
+    {
+        name: 'profile',
+        path: '/profile',
+        component: ProfilePage
+    }
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+    
+  });
+  
+  export default router;
